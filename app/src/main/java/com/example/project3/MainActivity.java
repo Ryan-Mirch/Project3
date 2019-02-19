b7a9aa49b7a8039cfec3d3eb8c56d88458ddbf0f
Ryan <rwmirch@gmail.com>
2019-02-18 21:59:11 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/project3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.project3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//changing code for commit
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
