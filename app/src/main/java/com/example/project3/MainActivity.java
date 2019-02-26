e5af6b94c14ede99a9a6d65f586742ae59749220
Ryan <rwmirch@gmail.com>
2019-02-25 23:15:38 -0500

    Changed settings to How to play. Made how to play button change to the how to play screen.

--- app/src/main/java/com/example/project3/MainActivity.java
+++ app/src/main/java/com/example/project3/MainActivity.java
@@ -1,12 +1,30 @@
 package com.example.project3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
+    private Button button_howToPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_howToPlay = findViewById(R.id.button_howToPlay);
+        button_howToPlay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+               openHowToPlayActivity();
+            }
+        });
+    }
+
+    public void openHowToPlayActivity(){
+        Intent intent = new Intent(this, HowToPlayActivity.class);
+        startActivity(intent);
     }
 }
